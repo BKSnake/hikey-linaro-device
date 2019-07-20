8051b1a92f9591c8322450306468a27dd258286f
Andrii Baliuk <zerksis.break@gmail.com>
2019-07-20 22:03:39 +0300

    kernel: Fix make build file.
    Change-Id: Ic6a7186e635878a82ddff7b24b5a23081ec02f2c
    Signed-off-by: Andrii Baliuk <zerksis.break@gmail.com>

--- hikey960.mk
+++ hikey960.mk
@@ -1,7 +1,16 @@
 ifndef TARGET_KERNEL_USE
 TARGET_KERNEL_USE=4.9
 endif
-PRODUCT_OUT_DIR := $(TARGET_OUT_ROOT)/product/hikey960
+
+$(warning OUT_DIR = '$(OUT_DIR)')
+ifeq (,$(strip $(OUT_DIR)))
+PRODUCT_OUT_DIR := out/target/product/hikey960
+else
+PRODUCT_OUT_DIR := $(OUT_DIR)/target/product/hikey960
+endif
+$(warning PRODUCT_OUT_DIR = '$(PRODUCT_OUT_DIR)')
+
+PRODUCT_OUT_DIR := $(OUT_DIR)/target/product/hikey960
 TARGET_PREBUILT_KERNEL := $(PRODUCT_OUT_DIR)/Image.gz-hikey960-$(TARGET_KERNEL_USE)
 TARGET_PREBUILT_DTB := $(PRODUCT_OUT_DIR)/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
 
